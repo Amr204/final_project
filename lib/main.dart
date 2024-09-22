6c6ff831d814f82cea8e169c9ab73e1cd6aa0c60
Amr204 <140981183+Amr204@users.noreply.github.com>
2024-09-22 12:03:06 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+//my first try
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+ 
+  runApp(
+    DevicePreview(
+      builder: (context)=> 
+       const MyApp()
+    )
+       );
 }
 
 class MyApp extends StatelessWidget {
